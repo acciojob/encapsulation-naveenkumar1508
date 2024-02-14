fa0007c40b43b36a11822b7d862e7e9699bf9deb
Naveenkumar <naveent1508@gmail.com>
2024-02-14 15:18:39 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly onj = new RWOnly();
+        obj.name = "Naveen Kumar";
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+
+    private String name;
+
 }
